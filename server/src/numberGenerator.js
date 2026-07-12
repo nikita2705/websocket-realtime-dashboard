e64c1ffce47db363d88502f952bdf15a73183a7e
unknown <nikita270504@inbox.lv>
2026-07-12 13:08:23 +0300

    feat: implement history file generation and restoration on server

--- server/src/numberGenerator.js
+++ server/src/numberGenerator.js
@@ -1,8 +1,19 @@
 const WebSocket = require('ws');
+const fs = require('fs');
 
 let previousNumber = null;
 const minNumber = 0;
 const maxNumber = 100;
+let historyArray = [];
+if (!fs.existsSync('./data/history.json')) {
+  historyArray = [];
+} else {
+  const file = fs.readFileSync('./data/history.json', 'utf8');
+  historyArray = JSON.parse(file);
+}
+if (Array.isArray(historyArray) && historyArray.length > 0) {
+  previousNumber = historyArray[historyArray.length - 1].previousNumber;
+}
 
 function startGenerating(wss) {
   setInterval(() => {
@@ -17,19 +28,26 @@ function startGenerating(wss) {
       const finalMin = Math.max(minNumber, floor);
 
       const rawNumber = Math.random() * (finalMax - finalMin) + finalMin;
-      previousNumber = Number(rawNumber.toFixed(1))
-
-      const data = {
-        time: new Date().toISOString(),
-        previousNumber,
-      };
-
-      wss.clients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN) {
-          client.send(JSON.stringify(data));
-        }
-      });
+      previousNumber = Number(rawNumber.toFixed(1));
     }
+    const data = {
+      time: new Date().toISOString(),
+      previousNumber,
+    };
+
+    historyArray.push(data);
+
+    if (historyArray.length > 15) {
+      historyArray = historyArray.slice(-15);
+    }
+
+    fs.writeFileSync('./data/history.json', JSON.stringify(historyArray));
+
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(data));
+      }
+    });
   }, 10000);
 }
 
